2438be3de7ba537320b04602c7a1a9d038076d80
Evgeniy <ekovalwork@gmail.com>
2019-10-28 11:01:38 +0200

    Write queries for tables creating

---
+++ db/db.php
@@ -0,0 +1,30 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'blog';
+$user = 'root';
+$password = 'root';
+
+class Db {
+    public $host;
+    public $dbname;
+    public $user;
+    public $password;
+
+    public function __construct($host, $dbname, $user=NULL, $password=NULL) {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function getConnection() {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+        $db = new PDO($dsn, $this->user, $this->password);
+        $db->exec("set names utf8");
+
+        return $db;
+    }
+}
+
+$db = new Db($host, $dbname, $user, $password);
